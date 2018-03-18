a976caecfaf792956d4701bbf90b8f2ef6623544
kwojtas07 <37503478+kwojtas07@users.noreply.github.com>
2018-03-18 13:42:23 +0100

    git init
    init

---
+++ PTM-HelloGit-Krasicki/PTMHelloGitKrasicki.cpp
@@ -0,0 +1,15 @@
+// PTMHelloGitKrasicki.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello Git" << endl;
+    return 0;
+}
+
